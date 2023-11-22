7e11518fb6956fa5a1114f73bf5c31dec9f74a24
NiladriModak <niladrimodakk2001@gmail.com>
2023-11-23 00:07:15 +0530

    packagee18

--- server.js
+++ server.js
@@ -17,14 +17,12 @@ app.use(cors({
     credentials:true,
     optionSuccessStatus: 200,
 }))
-
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://ad-hub-frontend-995v.vercel.app');
+res.header('Access-Control-Allow-Origin', '*');
+app.options('*', (req, res) => {
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
+    res.send();
   });
-
 
 app.use(helmet());
 app.set("trust proxy", 1);
